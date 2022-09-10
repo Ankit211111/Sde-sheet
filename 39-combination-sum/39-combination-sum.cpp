932f004a6e51b9ab6ca0b655b7687d5f53fa5d29
codereserves <108075873+codereserves@users.noreply.github.com>
2022-09-10 22:48:00 +0530

    Time: 179 ms (17.06%), Space: 52.4 MB (17.73%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void fun(int indx,vector<int>&ds,vector<vector<int>>&ans,vector<int>candidates,int target){
-        if(indx==candidates.size()){
+    void fun(int index,vector<int>ds,vector<vector<int>>&ans,vector<int>&arr,int target){
+        if(index==arr.size()){
             if(target==0){
                 ans.push_back(ds);
             }
             return;
         }
-        //taking part
-        if(candidates[indx]<=target){
-        ds.push_back(candidates[indx]);
-            //index value is not incremented because we can have same element unlimited times
-        fun(indx,ds,ans,candidates,target-candidates[indx]);
-            ds.pop_back();
+        if(arr[index]<=target){
+            ds.push_back(arr[index]);
+            fun(index,ds,ans,arr,target-arr[index]);
+                ds.pop_back();
         }
-        //not taking part
-        fun(indx+1,ds,ans,candidates,target);
+        fun(index+1,ds,ans,arr,target);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         vector<int>ds;
-       fun(0,ds,ans,candidates,target);
-         return ans;
+        vector<vector<int>>ans;
+        fun(0,ds,ans,arr,target);
+        return ans;
     }
 };
