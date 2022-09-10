2fb7c2d91384237b6836e18d61f4e1091f994a4d
codereserves <108075873+codereserves@users.noreply.github.com>
2022-09-10 21:59:36 +0530

    Time: 9 ms (75.42%), Space: 11.2 MB (57.38%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void fun(int indx,vector<int>&ds,vector<vector<int>>&ans,vector<int>&candidates,int target){
+        if(indx==candidates.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        //taking part
+        if(candidates[indx]<=target){
+        ds.push_back(candidates[indx]);
+            //index value is not incremented because we can have same element unlimited times
+        fun(indx,ds,ans,candidates,target-candidates[indx]);
+            ds.pop_back();
+        }
+        //not taking part
+        fun(indx+1,ds,ans,candidates,target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+       fun(0,ds,ans,candidates,target);
+         return ans;
+    }
+};
